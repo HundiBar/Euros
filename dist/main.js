ed32e9c676a21b6895b17df93389ef4409f5c7de
Dean Abbott <deanabbott293@gmail.com>
2021-06-20 23:20:31 +0900

    euros

---
+++ dist/main.js
@@ -0,0 +1,152 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// TODO: Autocomplete the input with AJAX calls.
+const teamNum = {
+  austria: 8,
+  belgium: 13,
+  croatia: 56370,
+  czech: 58837,
+  denmark: 35,
+  england: 39,
+  finland: 42,
+  france: 43,
+  germany: 47,
+  hungary: 57,
+  italy: 66,
+  netherlands: 95,
+  north: 59205,
+  poland: 109,
+  portugal: 110,
+  russia: 57451,
+  scotland: 117,
+  slovakia: 58836,
+  spain: 122,
+  sweden: 127,
+  switzerland: 128,
+  turkey: 135,
+  ukraine: 57166,
+  wales: 144,
+};
+
+const input = document.querySelector("#search");
+const results = document.querySelector("#results");
+
+const populateList = (data) => {
+  results.innerHTML = "";
+  const words = data.words;
+  words.forEach((suggestion) => {
+    results.insertAdjacentHTML('beforeend',
+      `
+      <a href="https://www.uefa.com/uefaeuro-2020/teams/${teamNum[suggestion]}--${suggestion}/">
+      <li class="list-group-item">${suggestion}</li></a>
+      `);
+  });
+};
+
+const suggestionGetter = (event) => {
+  event.preventDefault();
+  const url = `https://wagon-dictionary.herokuapp.com/autocomplete/${input.value}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => populateList(data));
+};
+input.addEventListener("keyup", suggestionGetter);
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=main.js.map
